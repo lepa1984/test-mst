a147d481dda363bef4b335614ab6b77aaec5e7f5
Алексей <62145338+lepa1984@users.noreply.github.com>
2025-11-14 16:59:57 +0300

    Delete js directory

--- js/header.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('scroll', () => {
-    if (scrollY > 0) {
-        document.querySelector('.header').classList.add('active');
-    }
-    if (scrollY == 0) {
-        document.querySelector('.header').classList.remove('active');
-    }
-});
--- js/sliders.js
+++
@@ -1,9 +0,0 @@
-const swiperMain = new Swiper('.main-swiper', {
-    spaceBetween: 16,
-    slidesPerView: 1,
-    pagination: {
-        el: '.main-swiper .swiper-pagination',
-        type: 'bullets',
-        clickable: true,
-    },
-});
